cf1e2dae0d8a7755878b9910eeecf554791bcdc0
Mohammad Hamrah <mhamrah96@gmail.com>
2026-05-12 19:07:33 -0400

    Update bulk-user-creation.ps1

--- scripts/bulk-user-creation.ps1
+++ scripts/bulk-user-creation.ps1
@@ -1,1 +1,29 @@
+Import-Module ActiveDirectory
 
+$users = @(
+    "John.Smith",
+    "Sarah.Jones",
+    "Mike.Brown",
+    "Emily.Davis",
+    "Chris.Wilson",
+    "John.Doe",
+    "Ryder.Matt",
+    "Frank.Paul",
+    "Mary.Washington",
+    "Omar.Phil"
+)
+
+foreach ($user in $users) {
+
+    $password = ConvertTo-SecureString "Password123!" -AsPlainText -Force
+
+    New-ADUser `
+        -Name $user `
+        -SamAccountName $user `
+        -UserPrincipalName "$user@maindomain.com" `
+        -AccountPassword $password `
+        -Enabled $true `
+        -ChangePasswordAtLogon $true `
+        -PasswordNeverExpires $false `
+        -Path "OU=IT,DC=maindomain,DC=com"
+}
